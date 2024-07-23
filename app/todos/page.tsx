3406dfab8ac96d564e19e50cd05bbeec03a2e3e1
vineeth-0509 <vineeththungani@gmail.com>
2024-07-23 10:17:13 +0530

    static side generation

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import revalidate from "../lib/actions/action1";
 
 export default async function Home() {
-  const response= await fetch("https:sum-server.100xdevs.com/todos");
+  const response= await fetch("https:sum-server.100xdevs.com/todos",{next:{ tags:['todos']}});
   const data= await response.json();
-  console.log("data found from the backend server is")
-  console.log(JSON.stringify(data));
+    revalidate()
 
   return (
    <div>
